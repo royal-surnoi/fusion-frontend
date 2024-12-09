1c1db201e7bb4c31b2fd92807991fbdab83a1634
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-12-09 12:55:55 +0530

    zone_id

--- terrafrom/ec2.tf
+++ terrafrom/ec2.tf
@@ -64,12 +64,12 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_route53_zone" "dev" {
-  name = "royalreddy.co.in"
-}
+# resource "aws_route53_zone" "dev" {
+#   name = "royalreddy.co.in"
+# }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.dev.zone_id
+  zone_id = "Z0353356K5UH2L31E18V"
   name    = "dev.royalreddy.co.in"
   type    = "A"
   ttl     = 300
